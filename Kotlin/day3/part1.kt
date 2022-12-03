8829cd30a3054287850ce123b269b7091ca79980
Mr. Pine <50425705+Mr-Pine@users.noreply.github.com>
2022-12-04 00:09:36 +0100

    Day 3

---
+++ Kotlin/day3/part1.kt
@@ -0,0 +1,25 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+
+    /*val inputString = """
+        vJrwpWtwJgWrhcsFMMfFFhFp
+        jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+        PmmdzqPrVvPwwTWBwg
+        wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+        ttgJtRGJQctTZtZT
+        CrZsJsPPZsGzwwsLwLmpwMDw
+    """.trimIndent()*/
+
+    val inputString = File("./src/main/resources/3-input.txt").readText().trim()
+
+    val result = inputString.lines().map { line ->
+        line.trim().let { Pair(it.substring(0 until it.length / 2), it.substring(it.length / 2)) }
+    }.map { (first, second) ->
+        first.find { second.contains(it) }!!.priority()
+    }.sum()
+
+    println(result)
+}
+
+fun Char.priority() = code - (if(isLowerCase()) 'a'.code else ('A'.code - 26)) + 1
---
+++ Kotlin/day3/part2.kt
@@ -0,0 +1,27 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+
+    /*val inputString = """
+        vJrwpWtwJgWrhcsFMMfFFhFp
+        jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+        PmmdzqPrVvPwwTWBwg
+        wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+        ttgJtRGJQctTZtZT
+        CrZsJsPPZsGzwwsLwLmpwMDw
+    """.trimIndent()*/
+
+    val inputString = File("./src/main/resources/3-input.txt").readText().trim()
+
+    val lines = inputString.lines()
+
+    val result = (0 until lines.size / 3).map { lines.subList(it * 3, it * 3 + 3) }.map {block ->
+        block[0].find {
+            block[1].contains(it) && block[2].contains(it)
+        }!!.priority()
+    }.sum()
+
+    println(result)
+}
+
+//fun Char.priority() = code - (if(isLowerCase()) 'a'.code else ('A'.code - 26)) + 1
