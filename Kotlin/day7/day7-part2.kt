5f4cef18b683cd48263a2f4bf7caba33f8d771d0
Mr. Pine <50425705+Mr-Pine@users.noreply.github.com>
2022-12-07 08:07:31 +0100

    Day 7

---
+++ Kotlin/day7/day7-part2.kt
@@ -0,0 +1,58 @@
+import common.AoCData
+
+fun main(args: Array<String>) {
+
+    val data = AoCData(
+        filePath = "./day7/input.txt",
+        """
+            ${'$'} cd /
+            ${'$'} ls
+            dir a
+            14848514 b.txt
+            8504156 c.dat
+            dir d
+            ${'$'} cd a
+            ${'$'} ls
+            dir e
+            29116 f
+            2557 g
+            62596 h.lst
+            ${'$'} cd e
+            ${'$'} ls
+            584 i
+            ${'$'} cd ..
+            ${'$'} cd ..
+            ${'$'} cd d
+            ${'$'} ls
+            4060174 j
+            8033020 d.log
+            5626152 d.ext
+            7214296 k
+        """,
+        exampleId = null
+    )
+
+    val directories = mutableMapOf<String, Int>()
+    val pathStack = mutableListOf<String>()
+
+    data.currentString().split("$").map(String::trim).filter(String::isNotBlank).forEach {
+        val commandArgs = it.lines()[0].split(" ")
+        if(commandArgs[0] == "cd") {
+            if(commandArgs[1] == "..") pathStack.removeLast()
+            else pathStack.add(commandArgs[1])
+        } else {
+            it.lines().drop(1).map { it.split(" ") }.filter { it[0] != "dir" }.map { it[0].toInt() }
+                .forEach {
+                        fileSize ->
+                    pathStack.indices.forEach { dirIndex ->
+                        val path = pathStack.subList(0, dirIndex + 1).joinToString("/")
+                        directories[path] = (directories[path] ?: 0) + fileSize
+                    }
+                }
+        }
+    }
+
+    println(directories)
+    println(directories.filter { it.value >= (directories["/"]!! - 40000000)}.minBy(Map.Entry<String, Int>::value))
+
+}
