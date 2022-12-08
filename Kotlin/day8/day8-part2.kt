f3e7bb8072366b9b418295cf00f5881c7a54e69e
Mr. Pine <50425705+Mr-Pine@users.noreply.github.com>
2022-12-08 18:54:31 +0100

    Day 8 oneliner

--- Kotlin/day8/day8-part2.kt
+++ Kotlin/day8/day8-part2.kt
@@ -17,10 +17,10 @@ fun main() {
     val result = data.lines().map { it.map { it.digitToInt() } }.let{ grid -> grid.mapIndexed { rowIndex, treeRow ->
         treeRow.mapIndexed { treeIndex, tree ->
             grid.map { it[treeIndex] }.let { column ->
-                val top = column.subList(0, rowIndex).reversed().takeIf { it.isNotEmpty() }?.let{ it.indexOfFirst { it >= tree }.takeIf { it != -1 }?.let { it + 1 } ?: it.size } ?: 0
-                val bottom = column.reversed().subList(0, column.lastIndex - rowIndex).reversed().takeIf { it.isNotEmpty() }?.let{ it.indexOfFirst { it >= tree }.takeIf { it != -1 }?.let { it + 1 } ?: it.size } ?: 0
-                val left = treeRow.subList(0, treeIndex).reversed().takeIf { it.isNotEmpty() }?.let{ it.indexOfFirst { it >= tree }.takeIf { it != -1 }?.let { it + 1 } ?: it.size } ?: 0
-                val right = treeRow.reversed().subList(0, column.lastIndex - treeIndex).reversed().takeIf { it.isNotEmpty() }?.let{ it.indexOfFirst { it >= tree }.takeIf { it != -1 }?.let { it + 1 } ?: it.size } ?: 0
+                val top = column.subList(0, rowIndex).reversed().visionDistance(tree)
+                val bottom = column.reversed().subList(0, column.lastIndex - rowIndex).reversed().visionDistance(tree)
+                val left = treeRow.subList(0, treeIndex).reversed().visionDistance(tree)
+                val right = treeRow.reversed().subList(0, column.lastIndex - treeIndex).reversed().visionDistance(tree)
                 top * bottom * left * right
             }
         }
@@ -29,3 +29,8 @@ fun main() {
     println(result)
 
 }
+
+fun List<Int>.visionDistance(treeHeight: Int) = takeIf { isNotEmpty() }?.let {
+    it.indexOfFirst { it >= treeHeight }
+        .takeIf { it != -1 }?.let { it + 1 } ?: it.size
+} ?: 0
