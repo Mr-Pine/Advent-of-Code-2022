52b7d876e4a745157aaa88ececa252f2d4f4d68e
Mr. Pine <50425705+Mr-Pine@users.noreply.github.com>
2022-12-12 12:27:37 +0100

    Day 12

---
+++ Kotlin/day12/day12-part1.kt
@@ -0,0 +1,45 @@
+import common.AoCData
+
+fun main() {
+
+    val data = AoCData(
+        filePath = "./day12/input.txt", """
+            Sabqponm
+            abcryxxl
+            accszExk
+            acctuvwj
+            abdefghi
+        """, exampleId = null
+    )
+
+    val grid = data.lines().map { it.asSequence().toMutableList() }
+
+    val start = grid.first { it.contains('S') }.let {
+        grid.indexOf(it) to it.indexOf('S')
+    }
+
+    val end = grid.first { it.contains('E') }.let {
+        grid.indexOf(it) to it.indexOf('E')
+    }
+
+    grid[start] = 'a'
+    grid[end] = 'z'
+
+    var paths = listOf(start to 0)
+
+    while (!paths.map(Pair<Pair<Int, Int>, Int>::first).contains(end)) {
+        paths = paths.map { (end, length) ->
+            end.surrounding().filter { it.first in 0..grid.lastIndex && it.second in 0..grid[0].lastIndex }.filter { grid[it].code - grid[end].code <= 1 }.map {
+                it to length + 1
+            }
+        }.flatten().groupBy { it.first }.values.map { it.minBy { it.second } }
+    }
+
+    println(paths.find { it.first == end }!!.second)
+
+}
+
+operator fun <T> List<List<T>>.get(pair: Pair<Int, Int>) = get(pair.first)[pair.second]
+operator fun <T> List<MutableList<T>>.set(pair: Pair<Int, Int>, value: T) = get(pair.first).set(pair.second, value)
+fun Pair<Int, Int>.surrounding() =
+    listOf(first - 1 to second, first + 1 to second, first to second + 1, first to second - 1)
---
+++ Kotlin/day12/day12-part2.kt
@@ -0,0 +1,53 @@
+import common.AoCData
+
+fun main() {
+
+    val data = AoCData(
+        filePath = "./day12/input.txt", """
+            Sabqponm
+            abcryxxl
+            accszExk
+            acctuvwj
+            abdefghi
+        """, exampleId = null
+    )
+
+    val grid = data.lines().map { it.asSequence().toMutableList() }
+
+    val start = grid.first { it.contains('S') }.let {
+        grid.indexOf(it) to it.indexOf('S')
+    }
+
+    val end = grid.first { it.contains('E') }.let {
+        grid.indexOf(it) to it.indexOf('E')
+    }
+
+    grid[start] = 'a'
+    grid[end] = 'z'
+
+    val lowest = grid.indices.map { row ->
+        grid[0].indices.map { row to it }
+    }.flatten().filter { grid[it] == 'a' }
+
+    var paths = listOf(end to 0)
+
+    while (!paths.any { it.first in lowest }) {
+        paths = paths.map { (pathEnd, length) ->
+            pathEnd.surrounding()
+                .filter { it.first in 0..grid.lastIndex && it.second in 0..grid[0].lastIndex }
+                .filter { grid[pathEnd].code - grid[it].code <= 1 }.map {
+                    it to length + 1
+                }
+        }.flatten().groupBy { it.first }.values.map { it.minBy { it.second } }
+    }
+
+    println(paths.find { it.first in lowest })
+
+}
+
+/*
+operator fun <T> List<List<T>>.get(pair: Pair<Int, Int>) = get(pair.first)[pair.second]
+operator fun <T> List<MutableList<T>>.set(pair: Pair<Int, Int>, value: T) = get(pair.first).set(pair.second, value)
+fun Pair<Int, Int>.surrounding() =
+    listOf(first - 1 to second, first + 1 to second, first to second + 1, first to second - 1)
+*/
